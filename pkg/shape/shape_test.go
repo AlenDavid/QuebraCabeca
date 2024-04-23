4ffb1a60d2e8a44400697d6cf38305778bf082cd
David Alen <davidalen.dev@gmail.com>
2024-04-23 14:57:44 -0300

    add: coverage on zero() method

--- pkg/shape/shape_test.go
+++ pkg/shape/shape_test.go
@@ -13,12 +13,26 @@ func TestZero(t *testing.T) {
 		Expected [2]int
 	}
 
-	cases := []scenario{
-		{"1,1", Shape{{1, 2}, {3, 0}}, [2]int{1, 1}},
-		{"1,0", Shape{{1, 2}, {0, 3}}, [2]int{1, 0}},
-	}
-
 	t.Run("can find zero", func(t *testing.T) {
+		cases := []scenario{
+			{"1,1", Shape{{1, 2}, {3, 0}}, [2]int{1, 1}},
+			{"1,0", Shape{{1, 2}, {0, 3}}, [2]int{1, 0}},
+		}
+
+		for _, c := range cases {
+			t.Run(c.Name, func(t *testing.T) {
+				i, j := c.Input.zero()
+
+				assert.Equal(t, c.Expected, [2]int{i, j})
+			})
+		}
+	})
+
+	t.Run("cannot find zero", func(t *testing.T) {
+		cases := []scenario{
+			{"", Shape{{1, 2}, {3, 4}}, [2]int{-1, -1}},
+		}
+
 		for _, c := range cases {
 			t.Run(c.Name, func(t *testing.T) {
 				i, j := c.Input.zero()
